08a8791a3dbe7b045717891e43c34ad26bd3a295
Madalina  ZAMFIRA <mzamfira@iqar-france.fr>
2021-04-22 11:25:39 +0200

    create query for 5 apollo launches in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client';
+import { render } from 'react-dom';
+import { ApolloProvider } from '@apollo/client/react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache()
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function ApolloLaunches() {
+  const { loading, error, data } = useQuery(gql`
+  {
+    launches(limit: 5) {
+      id
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.launches.map(({ id, launch_date_utc, launch_success, rocket, links, details }) => (
+    <div key={id}>
+      <ul>
+        <li>Date de lancement UTC: {launch_date_utc}</li>
+        <li>Statut du lancement: {launch_success}</li>
+        <li>Nom de la fusée: {rocket.rocket_name}</li>
+        <li>Lien YouTube: {links.video_link}</li>
+        <li>Détails: {details}</li>
+      </ul>
+    </div>
+  ));
+}
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+        <ApolloLaunches />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+render(<App />, document.getElementById("root"));
